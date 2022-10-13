376f914088dbb498881ffe129fc4308543c8699e
marcelamsler <marcel.amsler@gmail.com>
2022-10-13 11:59:38 +0200

    added tianshou agent

--- src/env/wrapped_quox_env.py
+++ src/env/wrapped_quox_env.py
@@ -1,9 +1,7 @@
 from pettingzoo.utils.conversions import turn_based_aec_to_parallel_wrapper
-from stable_baselines3.common.env_checker import check_env
 
 from env.qwox_env import QwoxEnv
 from pettingzoo.utils import wrappers
-import supersuit as ss
 from env.qwox_env import QwoxEnv
 
 
@@ -14,15 +12,15 @@ def wrapped_quox_env():
     env = wrappers.OrderEnforcingWrapper(env)
     return env
 
-def ss_wrapped_quox_env():
-    env_ = QwoxEnv()
-    #env_ = wrappers.CaptureStdoutWrapper(env_)
-    #env_ = wrappers.TerminateIllegalWrapper(env_, illegal_reward=-1)
-    #env_ = wrappers.OrderEnforcingWrapper(env_)
-    env_ = turn_based_aec_to_parallel_wrapper(env_)
-    env_ = ss.pettingzoo_env_to_vec_env_v1(env_)
-    env_ = ss.concat_vec_envs_v1(env_, 1, base_class="stable_baselines3")
-    return env_
+# def ss_wrapped_quox_env():
+#     env_ = QwoxEnv()
+#     #env_ = wrappers.CaptureStdoutWrapper(env_)
+#     #env_ = wrappers.TerminateIllegalWrapper(env_, illegal_reward=-1)
+#     #env_ = wrappers.OrderEnforcingWrapper(env_)
+#     env_ = turn_based_aec_to_parallel_wrapper(env_)
+#     env_ = ss.pettingzoo_env_to_vec_env_v1(env_)
+#     env_ = ss.concat_vec_envs_v1(env_, 1, base_class="stable_baselines3")
+#     return env_
 
 
 if __name__ == '__main__':
